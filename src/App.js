cb41a2cfd4e84192326911e36c906b8b1566e38d
shaAnder <shaunanderton1@gmail.com>
2025-03-18 12:16:13 +0000

    setup routing for pages and deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./App.css";
+import css from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className={css.App}>
       <NavBar />
+      <Container className={css.Main}>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/signin" element={<h1>Signin</h1>} />
+          <Route path="/signup" element={<h1>Signup</h1>} />
+          {/* Catch page not found */}
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import Icon from "./Icon";
 import css from "../css/NavBar.module.css";
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={css.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
@@ -14,16 +17,34 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link className={css.navLink}>
-              <Icon name="house" size={24} color="grey" />
+            <Nav.Link
+              as={Link}
+              to="/"
+              className={`${css.NavLink} ${
+                location.pathname === "/" ? css.Active : ""
+              }`}
+            >
+              <Icon name="house" size={24} />
               Home
             </Nav.Link>
-            <Nav.Link className={css.navLink}>
-              <Icon name="person" size={24} color="grey" />
+            <Nav.Link
+              as={Link}
+              to="/signin"
+              className={`${css.NavLink} ${
+                location.pathname === "/signin" ? css.Active : ""
+              }`}
+            >
+              <Icon name="person" size={24} />
               Sign In
             </Nav.Link>
-            <Nav.Link className={css.navLink}>
-              <Icon name="box-arrow-in-right" size={24} color="grey" />
+            <Nav.Link
+              as={Link}
+              to="/signup"
+              className={`${css.NavLink} ${
+                location.pathname === "/signup" ? css.Active : ""
+              }`}
+            >
+              <Icon name="box-arrow-in-right" size={24} />
               <span>Signup</span>
             </Nav.Link>
           </Nav>
