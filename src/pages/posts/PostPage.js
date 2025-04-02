7fd0e6fcabdc013576afd3fdd851f49c5189914c
shaAnder <shaunanderton1@gmail.com>
2025-04-02 16:43:02 +0100

    user can now view comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -30,10 +30,12 @@ function PostPage() {
     const handleMount = async () => {
       try {
         // Get the post data using the 'id' and set the state
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?posts=${id}`),
         ]);
         setPost({ results: [post] });
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -58,6 +60,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <p key={comment.id}>
+                {comment.owner}: {comment.content}
+              </p>
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
